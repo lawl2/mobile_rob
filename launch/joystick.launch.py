4bdf2741ecf693e71897a0bff196572aff0e39c5
lawl2 <roccodeciantis@outlook.com>
2023-01-12 09:44:39 +0100

    adding teleop with joystick and other mods

---
+++ launch/joystick.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    joy_params = os.path.join(get_package_share_directory('mobile_rob'),'config','joystick.yaml')
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params],
+         )
+
+    
+    teleop_node = Node(
+            package='teleop_twist_joy', 
+            executable='teleop_node',
+            name = 'teleop_node',
+            parameters=[joy_params],
+            remappings=[('/cmd_vel', '/diff_cont/cmd_vel_unstamped')]
+            )
+
+    return LaunchDescription([
+        joy_node,  
+        teleop_node    
+    ])
+
+
+    #to use on the robot:
+    # ros2 launch mobile_rob launch_sim.launch.py
+    #in another terminal
+    # ros2 launch mobile_rob joystick.launch.py
+    # to see the topic
+    # ros2 topic echo /diff_cont/cmd_vel_unstamped
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -19,17 +19,35 @@ def generate_launch_description():
 
     package_name='mobile_rob'
 
+    #per cambiare da gazebo_control a ros2_control
+    #metto true o false a use_ros2_control
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control' : 'true'}.items()  #modifico argomenti aggiungendo 'use_ros2_control'
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+
+    #                      PROBLEM 1
+    #to set the extra_gazebo_args using the gazebo_params.yaml
+    #which set the publish rate to 300.0Hz (not 10)
+    #used for Rviz to be smoother (problem error)
+    gazebo_params_path = os.path.join(
+                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
+         )
+
+
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    #gazebo = IncludeLaunchDescription(
+    #            PythonLaunchDescriptionSource([os.path.join(
+    #                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #         )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
